1fba4fede185f619a536114713061040d62d06c3
Daniel Lafontant <ldanieljajr@gmail.com>
2021-05-14 10:43:17 -0400

    alerts: add partials for flash alerts

--- models/user.js
+++ models/user.js
@@ -1,32 +1,6 @@
 'use strict';
 const bcrypt = require('bcrypt');
 
-// const {
-//   Model
-// } = require('sequelize');
-// module.exports = (sequelize, DataTypes) => {
-//   class user extends Model {
-//     /**
-//      * Helper method for defining associations.
-//      * This method is not a part of Sequelize lifecycle.
-//      * The `models/index` file will call this method automatically.
-//      */
-//     static associate(models) {
-//       // define association here
-//     }
-//   };
-//   user.init({
-//     name: DataTypes.STRING,
-//     email: DataTypes.STRING,
-//     password: DataTypes.STRING
-//   }, {
-//     sequelize,
-//     modelName: 'user',
-//   });
-//   return user;
-// };
-
-
 const {
   Model
 } = require('sequelize');
--- server.js
+++ server.js
@@ -55,17 +55,10 @@ app.use('/auth', require('./controllers/auth'));
 
 
 
-
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8000;
 const server = app.listen(PORT, () => {
   console.log(`🎧 You're listening to the smooth sounds of port ${PORT} 🎧`);
 });
 
-
-
-
-
-
-
-
 module.exports = server;
+
